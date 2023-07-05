327e92a457eed2299d20d3d6c170101285bb541f
anonWilder <austineokafor.e@gmail.com>
2023-07-05 23:23:34 +0100

    pushing for mails

--- LikeWise/settings.py
+++ LikeWise/settings.py
@@ -210,6 +210,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'artistictems@gmail.com'
-EMAIL_HOST_PASSWORD = '@#$tine.101'
+EMAIL_HOST_PASSWORD = 'jjipzpnvzrlheeuq'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
